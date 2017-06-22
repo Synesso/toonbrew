a4f792a34ac046680335f2e824c41b2c22564b31
Jem Mawson <jem.mawson@gmail.com>
2017-06-22 15:00:07 +1000

    publish to tar correctly

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@
 tar czvf pack.tgz img/ css index.html target/scala-2.12/*.js
 git stash
 git checkout site
-for f in `find` do git rm -r $f; done
+for f in `find`; do git rm -rf $f; done
 tar zxvf pack.tgz
-for f in `tar tvf pack.tgz`; do git add $f; done
+for f in `tar tf pack.tgz`; do git add $f; done
