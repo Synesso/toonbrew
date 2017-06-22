0ad0ce5941743b3a8d4e82a9233007beff560f78
Jem Mawson <jem.mawson@gmail.com>
2017-06-22 15:09:16 +1000

    host pages on master

--- publish.sh
+++ publish.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -x
 
-tar czvf pack.tgz img/ css index.html target/scala-2.12/*.js
-git stash
-git checkout site
-for f in `find`; do git rm -rf $f; done
-tar zxvf pack.tgz
-for f in `tar tf pack.tgz`; do git add -f $f; done
+cp target/scala-2.12/toon-brew*.js js/
+
